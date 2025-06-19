055ab17be78a63a8286c811e0e155328f52ad626
DaegeunChoe <asdf12gg@gmail.com>
2025-06-19 09:10:25 +0900

    프로젝트 생성, test_main 분리

---
+++ TextRPGProject/test/test_main.cpp
@@ -0,0 +1,10 @@
+// 빌드 설정을 'Debug', 'Release'가 아닌 'Test'으로 지정하면, 여기에 있는 main이 실행됩니다.
+// 이 파일은 수정사항이 git/github에 적용되지 않습니다.
+// 코드를 테스트해야할 일이 있다면, 여기에서 테스트 바랍니다.
+
+#include <iostream>
+
+int main() {
+	std::cout << "Hello, Test World!!!" << std::endl;
+	return 0;
+}
