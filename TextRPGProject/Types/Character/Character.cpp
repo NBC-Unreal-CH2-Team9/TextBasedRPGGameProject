86084ab951639aab5a40dabc7a799b327f79457c
DONGRYULO <rotem4587@knou.ac.kr>
2025-06-20 15:23:04 +0900

    character 초안

--- TextRPGProject/Types/Character/Character.cpp
+++ TextRPGProject/Types/Character/Character.cpp
@@ -1,1 +1,13 @@
 #include "Character.h"
+#include <iostream>
+
+void Character::DisplayStatus()
+{
+	std::cout << "===Character Status===" << std::endl;
+	std::cout << "Name: " << name << std::endl;
+}
+
+void LevelUp()
+{
+	
+}
--- TextRPGProject/Types/Character/Character.h
+++ TextRPGProject/Types/Character/Character.h
@@ -1,11 +1,31 @@
 #pragma once
 
 #include "../Actor.h"
+#include "../Item/Item.h"
 
 class Character : public Actor {
-	// 구현
 
 public:
+	Character(std::string name, int level = 1, int maxHealth = 200, int experience = 0, int gold = 0)
+		: Actor(name, maxHealth, 30)
+		, level(level)
+		, maxHealth(maxHealth)
+		, experience(experience)
+		, gold(gold)
+		, item(nullptr)
+	{
 
-	// 
+	};
+
+	~Character();
+
+	void DisplayStatus();
+
+protected:	
+	// TODO : 게임 매니저 클래스에서 캐릭터를 포인터로 들고 있게
+	int level;
+	int maxHealth;
+	int experience;
+	int gold;
+	Item* item;
 };
