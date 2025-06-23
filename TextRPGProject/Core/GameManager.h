3ae764ef2af8f9fd0ac0eeb333c67acbe8e4222b
DaegeunChoe <asdf12gg@gmail.com>
2025-06-23 09:19:46 +0900

    header 정리

--- TextRPGProject/Core/GameManager.h
+++ TextRPGProject/Core/GameManager.h
@@ -16,27 +16,23 @@ public:
 	GameManager();
 	~GameManager();
 
-	/* 필요하면 return type 추가 */
-	void CreateCharacter(/*필요하면 parameter 추가*/);
+	void CreateCharacter();
+	BattleResult Battle();
+	void Shop();
 
 private:
 	void GenerateMonster(int characterLevel);
 	void FightUntilDeath(Actor* attacker,Actor* defender);
 
-private:
+	void ShopBuy();
+	void ShopSell();
+
 	Monster* monsters[4];
 	int monsterNum;
 	int totalMonsterHealth;
 	int totalMonsterAttack;
 	bool isMyTurn;
 	bool isFighting;
-
-	/* 필요하면 return type 변경 */
-	BattleResult Battle(/*필요하면 parameter 추가*/);
-
-	void Shop();
-	void ShopBuy();
-	void ShopSell();
 
 	Character* GetCharacter() {
 		return character;
